91710047a225e5b09b0ee11b895d29e562eb117f
류경표 <kyongpyo0626@gmail.com>
2023-07-18 13:11:26 +0900

    string_and_hash

---
+++ PCCP/PCCP2/string_and_hash/make_equal_prequencies/make_equal_prequencies.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = []
+
+    return answer
+
+
+print(solution("aaabc"))
+print(solution("aabb"))
+print(solution("abcde"))
+print(solution("abcdeabc"))
+print(solution("abbccddee"))
---
+++ PCCP/PCCP2/string_and_hash/onetime_use/onetime_use.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+
+    return answer
+
+
+print(solution("statitsics"))
+print(solution("aabb"))
+print(solution("stringshowtime"))
+print(solution("abcdeabcdfg"))
