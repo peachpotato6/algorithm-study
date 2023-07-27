b593bc3761c999143184d8e8f76f984b08436073
류경표 <kyongpyo0626@gmail.com>
2023-07-28 00:57:06 +0900

    find_all_anagram fix break

--- hashmap_treeset/find_all_anagram/find_all_anagram.java
+++ hashmap_treeset/find_all_anagram/find_all_anagram.java
@@ -6,48 +6,42 @@ public class find_all_anagram {
 
     public int solution(String T, String S) {
         int answer = 0;
-
+    
         // S의 Hashmap
         HashMap<Character,Integer> Smap = new HashMap<>();
         for (char c : S.toCharArray()) {
             Smap.put(c,Smap.getOrDefault(c, 0)+1);
         }
-
+    
         // T의 Hashmap
         HashMap<Character,Integer> Tmap = new HashMap<>();
         char[] Tarr = T.toCharArray();
         int lt = 0, rt = S.length();
-
+    
         // rt까지 일단 Hashmap에 저장 (Sliding Window)
         for (int i = 0; i < rt; i++) {
             Tmap.put(Tarr[i],Tmap.getOrDefault(Tarr[i], 0)+1);
         }
-
-
-
+    
         while (rt <= T.length()){
             // equals로 하기
             // entrySet() 메소드는 Map의 키(key)와 값(value)을 모두 포함하는 Entry 객체의 집합(set)을 반환
             if (Tmap.entrySet().equals(Smap.entrySet())) {
                 answer++;
-                if (rt == T.length()) break;
             }
+            if (rt == T.length()) break;
             // 다음인덱스 +, 젤 작은 인덱스 -
             Tmap.put(Tarr[rt],Tmap.getOrDefault(Tarr[rt],0)+1);
-
-            // lt 의 조건
-            if (lt <= rt - S.length()) {
-                Tmap.put(Tarr[lt], Tmap.get(Tarr[lt]) - 1);
-                if (Tmap.get(Tarr[lt]) == 0) Tmap.remove(Tarr[lt]);
-                lt++;
-            }
-            
+    
+            Tmap.put(Tarr[lt], Tmap.get(Tarr[lt]) - 1);
+            if (Tmap.get(Tarr[lt]) == 0) Tmap.remove(Tarr[lt]);
+            lt++;
             rt++;
-            
         }
             
         return answer;
-}
+    }
+    
 
 
     public static void main(String args[]) {
