4be8398e726843ecbd4dc94e7c4384c1acdc14d4
류경표 <kyongpyo0626@gmail.com>
2023-07-28 18:20:12 +0900

    Kth_largest_number

---
+++ hashmap_treeset/Kth_largest_number/Kth_largest_number.java
@@ -0,0 +1,52 @@
+package hashmap_treeset.Kth_largest_number;
+
+import java.util.*;
+
+public class Kth_largest_number {
+
+    public int solution(int n, int k, int[] arr){
+        int answer = 0;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    map.put(arr[i]+arr[j]+arr[l],map.getOrDefault(arr[i]+arr[j]+arr[l], 0) + 1);
+                }
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            int max = 0;
+            for (Integer x : map.keySet()) {
+                if (max < x) {
+                    max = x;
+                }
+            }
+            if (i == k - 1) {
+                answer = max; 
+                break;
+            }
+            // k번째 수가 존재하지 않으면 -1
+            if (k > n) return -1;
+             map.remove(max);
+        }
+        return answer;
+
+    }
+
+    public static void main(String args[]){
+
+        Kth_largest_number T = new Kth_largest_number();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n,k,arr));
+        sc.close();
+    }
+    
+}
---
+++ hashmap_treeset/Kth_largest_number/Kth_largest_number_inflearn.java
@@ -0,0 +1,48 @@
+package hashmap_treeset.Kth_largest_number;
+
+import java.util.*;
+
+public class Kth_largest_number_inflearn {
+
+    public int solution(int n, int k, int[] arr){
+        int answer = -1;
+
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+
+        // i,j,l 모두 n까지로 해도됨
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    Tset.add(arr[i]+arr[j]+arr[l]);
+                    // Tset.remove()
+                    // Tset.first() -> 첫번째 꺼
+                    // Tset.last() -> 마지막 꺼
+                    // Tset.size()
+                    // Set은 중복제거고, Tree는 정렬(이진트리)이다.
+                }
+            }
+        }
+        int cnt = 0;
+        for (Integer x : Tset) {
+            cnt++;
+            if (cnt == k) return x;
+        }
+        return answer;
+
+    }
+
+    public static void main(String args[]){
+
+        Kth_largest_number_inflearn T = new Kth_largest_number_inflearn();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n,k,arr));
+        sc.close();
+    }
+    
+}
