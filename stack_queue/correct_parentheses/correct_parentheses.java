7753d2c83241392fe59821f0e2ee489666fd276b
류경표 <kyongpyo0626@gmail.com>
2023-07-29 09:28:57 +0900

    correct_parentheses_using_stack

---
+++ stack_queue/correct_parentheses/correct_parentheses.java
@@ -0,0 +1,32 @@
+package stack_queue.correct_parentheses;
+
+import java.util.Scanner;
+
+public class correct_parentheses {
+
+    public String solution(String s) {
+        String answer = "NO";
+
+        int a = 0, b = 0;
+        char[] arr = s.toCharArray();
+        for (char x : arr) {
+            if (x == '(') a++;
+            else b++;
+        }
+        if ((a == b) && (arr[s.length()-1] == ')')) {
+            return "YES";
+        }
+
+        return answer;
+    }
+
+    public static void main(String args[]) {
+
+        correct_parentheses T = new correct_parentheses();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(T.solution(s));
+        sc.close();
+    }
+    
+}
---
+++ stack_queue/correct_parentheses/correct_parentheses_inflearn.java
@@ -0,0 +1,37 @@
+package stack_queue.correct_parentheses;
+
+import java.util.*;
+
+public class correct_parentheses_inflearn {
+
+    public String solution(String s) {
+        String answer = "YES";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+
+            if (x == '(') stack.push(x);
+            else {
+                if (!stack.isEmpty()) stack.pop();
+                // stack이 비어있는데 ')'가 아직 있는 경우
+                else return "NO";
+            }
+        }
+        // stack에 아직 있는데 s가 끝난 경우
+        if (!stack.isEmpty()) return "NO";
+
+
+        return answer;
+    }
+
+    public static void main(String args[]) {
+
+        correct_parentheses_inflearn T = new correct_parentheses_inflearn();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(T.solution(s));
+        sc.close();
+    }
+    
+}
