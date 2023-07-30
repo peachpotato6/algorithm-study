947167014aa27dd18d41463ea2ec934c2c5fa1fb
류경표 <kyongpyo0626@gmail.com>
2023-07-30 10:53:50 +0900

    remove_parentheses_new_known

--- stack_queue/remove_parentheses/remove_parentheses.java
+++ stack_queue/remove_parentheses/remove_parentheses.java
@@ -29,3 +29,9 @@ public class remove_parentheses {
     }
 
 }
+// stream()
+// 컬렉션(Collection)과 배열(Array) 등의 데이터 소스를 다루는데 유용한 API이며 스트림은 각각의 원소를 순차적으로 처리하며, 병렬 처리도 가능하다.
+
+// collect()
+// 스트림의 각 원소를 하나의 결과 컨테이너에 수집하는 최종 연산이다.
+// 이 때 결과 컨테이너는 Supplier, Accumulator, Combiner 세 가지 인터페이스를 구현한 객체로 정의됩니다.
