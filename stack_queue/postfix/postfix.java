1ecf157b6a235088da137dab84a07441f34a1995
류경표 <kyongpyo0626@gmail.com>
2023-08-01 08:45:34 +0900

    postfix

---
+++ stack_queue/postfix/postfix.java
@@ -0,0 +1,55 @@
+package stack_queue.postfix;
+import java.util.*;
+
+public class postfix{
+
+    public int solution(String s){
+        int answer=0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (char x : s.toCharArray()){
+            if (Character.isDigit(x)){
+                stack.push(Character.getNumericValue(x));
+            }
+            else if (x == '+'){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                answer = num1 + num2;
+                stack.push(answer);
+            }
+            else if (x == '-'){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                answer = num1 - num2;
+                stack.push(answer);
+            }
+            else if (x == '*'){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                answer = num1 * num2;
+                stack.push(answer);
+            }
+            else if (x == '/'){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                answer = num1 / num2;
+                stack.push(answer);
+            }
+        }
+           
+        
+
+        return answer;
+    }
+
+    public static void main(String args[]){
+
+        postfix T = new postfix();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(T.solution(s));
+        sc.close();
+    }
+}
+
