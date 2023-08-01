521e2b3beab49176ec9fe9e5112263aa70b76a04
류경표 <kyongpyo0626@gmail.com>
2023-08-01 10:16:53 +0900

    postfix_inflearn

---
+++ stack_queue/postfix/postfix_inflearn.java
@@ -0,0 +1,36 @@
+package stack_queue.postfix;
+import java.util.*;
+
+public class postfix_inflearn{
+
+    public int solution(String s){
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            // '0' 이 48임
+            // '5'를 넣고싶으면 5가 들어감 // 53 - 48
+            if(Character.isDigit(x)) stack.push(x-48);
+            else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+
+                if (x == '+') stack.push(lt+rt);
+                else if (x == '-') stack.push(lt-rt);
+                else if (x == '*') stack.push(lt*rt);
+                else if (x == '/') stack.push(lt/rt);
+            }
+        }
+        return stack.get(0);
+    }
+
+    public static void main(String args[]){
+
+        postfix_inflearn T = new postfix_inflearn();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(T.solution(s));
+        sc.close();
+    }
+}
+
