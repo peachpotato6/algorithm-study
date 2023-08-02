006e3508d3a2137dbcbe116ce0ad63e92089152a
류경표 <kyongpyo0626@gmail.com>
2023-08-02 13:25:27 +0900

    iron_rod

---
+++ stack_queue/iron_rod/iron_rod.java
@@ -0,0 +1,42 @@
+package stack_queue.iron_rod;
+
+import java.util.*;
+
+public class iron_rod {
+
+    public int solution(String s) {
+        int answer= 0;
+
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (arr[i] == '(') stack.push(arr[i]);
+            else {
+                // Àü¿¡²¨ '(' ÀÌ¸é
+                if (arr[i-1] == '(') {
+                    stack.pop();
+                    answer += stack.size();
+                }
+                else {
+                    stack.pop();
+                    answer += 1;
+                }
+                
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String args[]) {
+
+        iron_rod T = new iron_rod();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(T.solution(s));
+
+
+    }
+    
+}
+ 
