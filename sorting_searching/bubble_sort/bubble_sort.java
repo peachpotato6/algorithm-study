eb4341dc205e1f8d7f2233bf4bbd30a27bc587c2
류경표 <kyongpyo0626@gmail.com>
2023-08-06 18:36:53 +0900

    bubble_sort

---
+++ sorting_searching/bubble_sort/bubble_sort.java
@@ -0,0 +1,32 @@
+    package sorting_searching.bubble_sort;
+
+    import java.util.Scanner;
+
+    // 버블 정렬(Bubble Sort)은 인접한 두 개의 원소를 비교하여 정렬하는 알고리즘
+    public class bubble_sort {
+
+        public static void main(String args[]) {
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n-1; j++) {
+                    if (arr[j] > arr[j+1]) {
+                        int tmp = arr[j+1];
+                        arr[j+1] = arr[j];
+                        arr[j] = tmp;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            sc.close();
+        }
+        
+    }
