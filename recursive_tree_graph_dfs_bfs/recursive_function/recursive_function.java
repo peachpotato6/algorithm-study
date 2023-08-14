083adcb43ac30b015b3cdb363816a325abaf19c6
류경표 <kyongpyo0626@gmail.com>
2023-08-14 20:51:29 +0900

    DFS_easy

---
+++ recursive_tree_graph_dfs_bfs/recursive_function/recursive_function.java
@@ -0,0 +1,33 @@
+// 3 입력시 1 2 3 출력
+
+package recursive_tree_graph_dfs_bfs.recursive_function;
+
+import java.util.*;
+
+public class recursive_function {
+
+    // stack frame
+    // back tracking
+    public void DFS(int n) {
+
+        if (n == 0) return;
+        else {
+            DFS(n-1);
+            System.out.print(n + " ");
+        }
+        
+        
+
+    }
+
+    public static void main(String args[]) {
+
+        recursive_function T = new recursive_function();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        T.DFS(n);
+        sc.close();
+
+    }
+    
+}
