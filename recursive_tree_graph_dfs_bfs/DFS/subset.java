c9f8c70df5118381c2629413c377e557dc5030ea
류경표 <kyongpyo0626@gmail.com>
2023-08-18 11:18:09 +0900

    DFS_subset

---
+++ recursive_tree_graph_dfs_bfs/DFS/subset.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class subset {
+
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+
+        if (L == n+1) {
+            // ch에 1로 체크된 인덱스 출력
+            for (int i = 1; i <= n; i++) { 
+                if (ch[i] == 1) System.out.print(i + " ");
+            }
+            System.out.println();
+
+        }
+        else {
+            // 사용
+            ch[L] = 1;
+            // 왼쪽
+            DFS(L+1);
+            // 사용 X
+            ch[L] = 0;
+            // 오른쪽
+            DFS(L+1);
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        subset T = new subset();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        // ch배열의 인덱스 번호가 원소
+        ch = new int[n+1];
+        T.DFS(1);
+    }
+    
+}
