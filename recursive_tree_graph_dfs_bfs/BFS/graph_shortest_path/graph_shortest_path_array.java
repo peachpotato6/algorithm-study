b59f0f0b8588b7df84707e9b3e8f63abdb1a8423
류경표 <kyongpyo0626@gmail.com>
2023-09-09 14:17:13 +0900

    using ArrayList<ArrayList<Integer>>

---
+++ recursive_tree_graph_dfs_bfs/BFS/graph_shortest_path/graph_shortest_path_array.java
@@ -0,0 +1,58 @@
+package graph_shortest_path;
+
+import java.util.*;
+
+public class graph_shortest_path_array {
+
+    static int n,m;
+    static ArrayList<ArrayList<Integer>> graph; 
+    static int[] ch, dis;
+
+    public void BFS(int v) {
+        Queue<Integer> Q = new LinkedList<>();
+        ch[v] = 1;
+        dis[v] = 0;
+        Q.offer(v);
+        while(!Q.isEmpty()) {
+            int cv = Q.poll();
+            for (int nv : graph.get(cv)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    Q.offer(nv);
+                    // + Level
+                    dis[nv] = dis[cv] + 1;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String args[]) {
+
+        graph_shortest_path_array T = new graph_shortest_path_array();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        // ArrayList ¸¸µé±â
+        graph = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < m; i++) {
+            graph.get(sc.nextInt()).add(sc.nextInt());
+        }
+        ch = new int[n+1];
+        dis = new int[n+1];
+
+        T.BFS(1);
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " : " + dis[i]);
+        }
+
+        sc.close();
+    }
+    
+}
