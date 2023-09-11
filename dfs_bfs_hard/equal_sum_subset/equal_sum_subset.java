6677a52178ee0bdbffdae19150a5730583aa7f85
류경표 <kyongpyo0626@gmail.com>
2023-09-11 15:36:05 +0900

    equal_sum_subset

---
+++ dfs_bfs_hard/equal_sum_subset/equal_sum_subset.java
@@ -0,0 +1,44 @@
+package dfs_bfs_hard.equal_sum_subset;
+import java.util.*;
+public class equal_sum_subset {
+
+    static String answer = "NO";
+    static int n, total = 0;
+    static int[] arr;
+    boolean flag = false;
+
+    public void DFS(int L, int sum) {
+
+        if (flag) return;
+        if (sum > total/2) return;
+        if (L == n) {
+            if (sum == (total-sum)) {
+                answer = "YES";
+                // 답 찾았으면 스택에 남아있는 DFS들 다 return
+                flag = true;
+            }
+        }
+        else {
+            // 두 갈래로 뻗기
+            DFS(L+1, sum+arr[L]);
+            DFS(L+1, sum);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        equal_sum_subset T = new equal_sum_subset();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            total += arr[i];
+        }
+        T.DFS(0,0);
+        System.out.println(answer);
+        sc.close();
+    }
+
+    
+}
