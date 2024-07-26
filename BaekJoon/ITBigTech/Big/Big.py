c7e2d4ca541a20b8b3b6e6fd644dd42445b91bf5
류경표 <kyongpyo0626@gmail.com>
2024-07-26 16:53:53 +0900

    Big

--- BaekJoon/ITBigTech/Big/Big.py
+++ BaekJoon/ITBigTech/Big/Big.py
@@ -2,16 +2,16 @@ N = int(input())
 
 people = [list(map(int, input().split())) for _ in range(N)]
 
+# 순위 리스트
+ranks = [1] * N
 
 for i in range(N):
     rank = 1
     for j in range(N):
         if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
-            rank = rank + 1
-    if i == N-1:
-        print(rank)
-    else:
-        print(rank, end=" ")
+            ranks[i] += 1
+
+print(" ".join(map(str, ranks)))
     
 
             
