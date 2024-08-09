04124defcc23c6d61b30793afd1cdcf47ffa8b3a
류경표 <kyongpyo0626@gmail.com>
2024-08-09 15:56:35 +0900

    CrossCountry

--- BaekJoon/ITBigTech/CrossCountry/CrossCountry.py
+++ BaekJoon/ITBigTech/CrossCountry/CrossCountry.py
@@ -1,35 +1,47 @@
 T = int(input())
-scoreList = dict()
-freqList = dict()
+score_list = dict()
+freq_list = dict()
+five_list = dict()
 
 for _ in range(T):
     N = int(input())
     table = list(map(int, input().split()))
     # add frequency
     for i in range(0, len(table)):
-        freqList[table[i]] = 0
+        freq_list[table[i]] = 0
     for i in range(0, len(table)):
-        freqList[table[i]] += 1
+        freq_list[table[i]] += 1
     
     # skip below 6
-    for k in freqList.keys():
-        if freqList[k] < 6:
+    for k in freq_list.keys():
+        if freq_list[k] < 6:
             table[:] = (val for val in table if val != k)
     for i in range(0, len(table)):
-        scoreList[table[i]] = 0
+        score_list[table[i]] = 0
+
+    # freqList 초기화해서 4번째 빈도까지 저장하는 리스트로 사용    
     for i in range(0, len(table)):
-        scoreList[table[i]] += i
+        freq_list[table[i]] = 0
     
+    # 4번째까지만 더해야함
+    for i in range(0, len(table)):
+        if freq_list[table[i]] < 4:
+            score_list[table[i]] += i+1
+            freq_list[table[i]] += 1
+        # 5번째 five_list에 저장
+        if freq_list[table[i]] == 4:
+            five_list[table[i]] = i+1
+        
     # winner
-    for items in scoreList.items():
-        max = 0
-        if max > items[1]:
-            max = items[1]
-        elif max == items[1]:
+    for items in score_list.items():
+        # 5번째랑 비교해서 5번째 점수가 높은 items[1]을 출력
+        if max(set(score_list.values())) == items[1]:
+            print(max(five_list, key=five_list.get))
+        else:
+            print(max(score_list, key=score_list.get))
 
-
-print(freqList.items())
-print(scoreList.items())
+print(freq_list.items())
+print(score_list.items())
 print(table)
 
 
